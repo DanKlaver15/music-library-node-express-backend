58809cad14c12862bbbbd52fc961a8abf1b42c38
DKlaver <dklaver15@gmail.com>
2021-02-22 20:02:03 -0600

    Added 2 GET endpoints; 1 for all songs and 1 for retrieving a single song by ID.

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Start server on port 3000
 app.listen(3000, function () {
 	console.log("Server started. Listening on port 3000.");
 });
+
+/*======================================================================*/
+
+// GET endpoints
+app.get('/api/songs', (req, res) => {
+	const products = repoContext.songs.findAllSongs();
+	return res.send(songs);
+});
+
+// Get a specific product by ID
+app.get('/api/songs/:id', (req, res) => {
+	const id = req.params.id;
+	const product = repoContext.songs.findSongById(id);
+	return res.send(song);
+});
+
