5628e1addc11a4eb914a91d82467319f5183ded7
DKlaver <dklaver15@gmail.com>
2021-02-22 20:15:55 -0600

    Updated both GET endpoints to use the proper repoContext.

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.listen(3000, function () {
 /*======================================================================*/
 
 // GET endpoints
-app.get('/api/songs', (req, res) => {
-	const products = repoContext.songs.findAllSongs();
+app.get('/api/music', (req, res) => {
+	const songs = repoContext.songs.findAllSongs();
 	return res.send(songs);
 });
 
 // Get a specific product by ID
-app.get('/api/songs/:id', (req, res) => {
+app.get('/api/music/:id', (req, res) => {
 	const id = req.params.id;
-	const product = repoContext.songs.findSongById(id);
-	return res.send(song);
+	const songs = repoContext.songs.findSongById(id);
+	return res.send(songs);
 });
 
